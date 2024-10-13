a3fb727b1a6e3d62ee7fa8431e6d8ea7e59ac749
Idrees-28 <idreesjee2810@gmail.com>
2024-10-13 18:11:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Img_section from "./components/Img_section"
+function App(){
+    return(<> 
+        <Img_section></Img_section>
+       
+</>)
+}
+  
+ export default App
---
+++ src/components/Img_section.js
@@ -0,0 +1,55 @@
+import img_1 from "../assets/images/img1.jpeg"
+import img_2 from "../assets/images/img2.jpeg"
+import img_3 from "../assets/images/img3.jpeg"
+import img_4 from "../assets/images/img4.jpeg"
+import img_5 from "../assets/images/img5.jpeg"
+import img_6 from "../assets/images/img6.jpeg"
+import img_7 from "../assets/images/img7.jpeg" 
+import img_8 from "../assets/images/img8.jpeg"
+
+ 
+
+function Img_section(){
+    return(
+        <div class="img_section">
+        <div class="images">
+            <img   class="img1" src= {img_1} alt=" smile"></img>
+            <p> Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_2} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_3} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_4} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_5} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_6} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_7} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+        <div class="images">
+            <img   class="img1" src={img_8} alt=" smile"></img>
+            <p>Inner beauty begins with a smile.</p>
+        </div>
+    
+       
+
+    </div>
+    
+    )
+  }
+
+  export default Img_section
